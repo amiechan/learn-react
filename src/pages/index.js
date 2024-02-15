7c606b39a4c0dfc9517f9a61c06347c0275fd008
amy <chan.amy@northeastern.edu>
2024-02-15 16:24:38 -0500

    class activity 02152024

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,21 +6,31 @@ import styles from '@/styles/Home.module.css'
 import Profile from './qcomps/profile_mistake'
 import MyComp from './qcomps/firstcomp'
 import TodoList from './qcomps/todos'
-import Gallery from './qcomps/gallery_props'
+// import Gallery from './qcomps/gallery_props'
 import PackingList from './qcomps/props_item'
 import List from './qcomps/list_keys_id'
 import RecipeList from './qcomps/recipes'
+// import Gallery from './components/whyState'
+// import Gallery from './components/state'
+import Gallery from './qcomps/state'
+import Form from './qcomps/stuckForm'
+import FeedbackForm from './qcomps/thankYouCrash'
+import RequestTracker from './qcomps/shoppingCart'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        {/* <Profile /> */}
-        {/* <MyComp /> */}
-        {/* <TodoList /> */}
-        {/* <Gallery /> */}
-        {/* <PackingList /> */}
-        {/* <List /> */}
-        <RecipeList />
+      {/* <Profile /> */}
+      {/* <MyComp /> */}
+      {/* <TodoList /> */}
+      {/* <Gallery /> */}
+      {/* <PackingList /> */}
+      {/* <List /> */}
+      {/* <RecipeList /> */}
+      {/* <Gallery /> */}
+      {/* <Form /> */}
+      {/* <FeedbackForm /> */}
+      <RequestTracker />
     </div>
   )
 }
